f0aa3492373a9956ab78424a98adf78c7a5e65e8
Cyril <ccrozat@artfx.fr>
2020-11-05 14:37:56 +0100

    LeftBarImplementation

--- PongGame/PongGame/Game1.cs
+++ PongGame/PongGame/Game1.cs
@@ -8,26 +8,51 @@ namespace PongGame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        Texture2D texture;
-        Vector2 position;
+
+        public int screenSizeX;
+        public int screenSizeY;
+        public float mousePositionY;
+
+        Texture2D ballTexture;
+        Vector2 ballPosition;
+        public int ballSizeX;
+        public int ballSizeY;
+
+        Texture2D leftBarTexture;
+        Vector2 leftBarPosition;
+        public int leftBarSizeX;
+        public int leftBarSizeY;
+
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-            position = new Vector2(0, 0);
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            texture = new Texture2D(this.GraphicsDevice, 100, 100);
-            Color[] colorData = new Color[100 * 100];
-            for (int i = 0; i < 10000; i++)
-                colorData[i] = Color.Red;
+            screenSizeX = this.GraphicsDevice.Viewport.Width;
+            screenSizeY = this.GraphicsDevice.Viewport.Height;
 
-            texture.SetData<Color>(colorData);
+            ballSizeX = 14;
+            ballSizeY = 14;
+            ballPosition = new Vector2(screenSizeX / 2 - ballSizeX / 2, screenSizeY / 2 - ballSizeY / 2);
+            ballTexture = new Texture2D(this.GraphicsDevice, ballSizeX, ballSizeY);
+            Color[] ballColorData = new Color[ballSizeX * ballSizeY];
+            for (int i = 0; i < ballSizeX * ballSizeY; i++)
+                ballColorData[i] = Color.White;
+            ballTexture.SetData<Color>(ballColorData);
+
+            leftBarSizeX = 20;
+            leftBarSizeY = 150;
+            leftBarPosition = new Vector2(screenSizeX / 17 - leftBarSizeX / 2, screenSizeY / 2 - leftBarSizeY / 2);
+            leftBarTexture = new Texture2D(this.GraphicsDevice, leftBarSizeX, leftBarSizeY);
+            Color[] leftBarColorData = new Color[leftBarSizeX * leftBarSizeY];
+            for (int i = 0; i < leftBarSizeX * leftBarSizeY; i++)
+                leftBarColorData[i] = Color.White;
+            leftBarTexture.SetData<Color>(leftBarColorData);
 
             base.Initialize();
         }
@@ -46,9 +71,13 @@ namespace PongGame
                 if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                     Exit();
 
-                position.X += 1;
-                if (position.X > this.GraphicsDevice.Viewport.Width)
-                    position.X = 0;
+                /*ballPosition.X += 1;
+                if (ballPosition.X > this.GraphicsDevice.Viewport.Width)
+                    ballPosition.X = 0;*/
+                mousePositionY = Mouse.GetState().Y;
+                leftBarPosition.Y = mousePositionY - ballSizeY / 2;
+                if (leftBarPosition.Y < 0) leftBarPosition.Y = 0;
+                if (leftBarPosition.Y + leftBarSizeY > screenSizeY) leftBarPosition.Y = screenSizeY - leftBarSizeY;
 
 
                 base.Update(gameTime);
@@ -61,7 +90,8 @@ namespace PongGame
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _spriteBatch.Draw(texture, position, Color.Red);
+            _spriteBatch.Draw(ballTexture, ballPosition, Color.White);
+            _spriteBatch.Draw(leftBarTexture, leftBarPosition, Color.White);
             _spriteBatch.End();
 
             base.Draw(gameTime);
